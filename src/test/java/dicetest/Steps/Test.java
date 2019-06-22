b909d378e81ba2c3b7e06caf5715d80d1ae4c336
Spoxmen <spoxmen@gmail.com>
2019-06-22 13:27:46 +0200

    cucumber partially added continuation v2

--- src/test/java/dicetest/Steps/Test.java
+++ src/test/java/dicetest/Steps/Test.java
@@ -6,7 +6,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(strict = false, format = { "pretty", "html:target/cucumber", "json:target/cucumber.json"}, monochrome = true)
+@CucumberOptions(strict = false, format = {
+		"pretty", 
+		"html:target/cucumber", 
+		"json:target/cucumber.json",
+		}, monochrome = true)
 
 
 public class Test {
